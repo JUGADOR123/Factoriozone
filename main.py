6dad2202985f453ed4b0fc4028fca2031e68dcb4
Jugador <momocordova@gmail.com>
2022-03-14 16:18:55 -0600

    async stuff plus hardcoded login

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ if __name__ == "__main__":
     logger = logging.getLogger()
     coloredlogs.install(level='DEBUG', logger=logger, fmt=f"[%(module)-1s]|[%(levelname)-1s]| %(message)s", )
     logger.info("Starting Client")
-    socket = ws.WebSocket()
-    asyncio.run(socket.start())
-    time.sleep(10)
-    socket.stop()
+    loop= asyncio.get_event_loop()
+    loop.run_until_complete(ws.receive("factorio.zone/ws"))
+    loop.run_forever()
+
+
--- src/websocket.py
+++ src/websocket.py
@@ -2,28 +2,31 @@ import asyncio
 import json
 import logging
 
+import aiohttp
 import websockets
 
 
-class WebSocket:
-    def __init__(self):
-        self.uri = "wss://factorio.zone/ws"
-        self.active: bool = False
+async def receive_handle(websocket: str):
+    async for message in websocket:
+        await handle_message(message)
 
-    async def __connect(self):
-        async with websockets.connect(self.uri) as websocket:
-            async for message in websocket:
 
-                message = json.loads(message)
-                if message['secret']:
-                    logging.info(f"Session ID Received: {message['secret']}")
+async def receive(websocket: str):
+    async with websockets.connect(f"wss://{websocket}") as websocket:
+        await receive_handle(websocket)
 
-    async def start(self):
-        logging.info("Starting websocket")
-        self.active = True
-        await self.__connect()
 
-    async def stop(self):
-        logging.info("Stopping websocket")
-        self.active = False
-        # await self.__connect()
+async def handle_message(message: str):
+    message = json.loads(message)
+    if "secret" in message:
+        logging.info(f"Client Secret Received: {message['secret']}")
+        #await login("SxIPKrHPEKNqKkDiyOKAHOic", message['secret'])
+    else:
+        logging.info(f"Client Message Received: {message}")
+
+
+async def login(user_token: str, Session_secret: str):
+    async with aiohttp.ClientSession() as session:
+        async with session.post("https://factorio.zone/api/user/login",  data={"userToken": user_token, "visitSecret": Session_secret,"reconnected":False,"script":"https://factorio.zone/cache/main.355cce551d992ff91c29.js"}) as response:
+            response = await response.json()
+            logging.info(f"Login Response: {response}")
