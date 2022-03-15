00fad4180516721f59ea13e2df2240b95d6e4547
FIRE-WATER799 <haywood2008918@outlook.com>
2022-03-14 19:40:17 -0500

    Added Requirements

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import asyncio, logging, time, discord, coloredlogs
+import asyncio, logging, time, discord, coloredlogs, json
 from discord.ext import commands
 from discord_slash import SlashCommand, SlashContext
 from dinteractions_Paginator import Paginator
