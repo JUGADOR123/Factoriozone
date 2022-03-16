a54d2e31e47477d5542e030de2c289de98d747e2
FIRE-WATER799 <57648521+FIRE-WATER799@users.noreply.github.com>
2022-03-15 19:54:45 -0500

    update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import asyncio, logging, time, discord, coloredlogs, json
 from discord.ext import commands
 from discord_slash import SlashCommand, SlashContext
+from discord_slash.utils.manage_commands import create_choice, create_option
 from dinteractions_Paginator import Paginator
 
 import src.websocket as ws
